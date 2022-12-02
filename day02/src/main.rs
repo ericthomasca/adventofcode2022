59cd3d604aef0bc3b36042056c0f6383365c718d
Eric Thomas <eric@ericthomas.ca>
2022-12-02 20:07:57 -0330

    day 2 complete

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -7,17 +7,17 @@ fn main() -> Result<()> {
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     let rounds: Vec<&str> = contents.split('\n').collect();
-    
-    
-    let my_total_score = strategy_part_1(rounds);
-    
-    println!("My total score is {}", my_total_score);
+
+    let my_part_1_score = strategy_part_1(&rounds);
+    let my_part_2_score = strategy_part_2(&rounds);
+
+    println!("My first strategy score is {}", my_part_1_score);
+    println!("My second strategy score is {}", my_part_2_score);
 
     Ok(())
-
 }
 
-fn strategy_part_1(rounds: Vec<&str>) -> i32 {
+fn strategy_part_1(rounds: &Vec<&str>) -> i32 {
     let mut my_total_score = 0;
     for round in rounds {
         let hands: Vec<&str> = round.split(' ').collect();
@@ -33,32 +33,64 @@ fn strategy_part_1(rounds: Vec<&str>) -> i32 {
 
         if opponent_hand == opponent_rock && my_hand == my_rock {
             my_score += 4;
-        }
-        else if opponent_hand == opponent_rock && my_hand == my_paper {
+        } else if opponent_hand == opponent_rock && my_hand == my_paper {
             my_score += 8;
-        }
-        else if opponent_hand == opponent_rock && my_hand == my_scissors {
+        } else if opponent_hand == opponent_rock && my_hand == my_scissors {
             my_score += 3;
-        }
-        else if opponent_hand == opponent_paper && my_hand == my_rock {
+        } else if opponent_hand == opponent_paper && my_hand == my_rock {
             my_score += 1;
-        }
-        else if opponent_hand == opponent_paper && my_hand == my_paper {
+        } else if opponent_hand == opponent_paper && my_hand == my_paper {
             my_score += 5;
-        }
-        else if opponent_hand == opponent_paper && my_hand == my_scissors {
+        } else if opponent_hand == opponent_paper && my_hand == my_scissors {
             my_score += 9;
-        }
-        else if opponent_hand == opponent_scissors && my_hand == my_rock {
+        } else if opponent_hand == opponent_scissors && my_hand == my_rock {
             my_score += 7;
-        }
-        else if opponent_hand == opponent_scissors && my_hand == my_paper {
+        } else if opponent_hand == opponent_scissors && my_hand == my_paper {
             my_score += 2;
-        }
-        else if opponent_hand == opponent_scissors && my_hand == my_scissors {
+        } else if opponent_hand == opponent_scissors && my_hand == my_scissors {
             my_score += 6;
+        } else {
+            println!("Problem mathing hand")
         }
-        else {
+
+        my_total_score += my_score;
+    }
+    my_total_score
+}
+
+fn strategy_part_2(rounds: &Vec<&str>) -> i32 {
+    let mut my_total_score = 0;
+    for round in rounds {
+        let hands: Vec<&str> = round.split(' ').collect();
+        let opponent_hand = hands[0];
+        let my_hand = hands[1];
+        let mut my_score = 0;
+        let opponent_rock = "A";
+        let opponent_paper = "B";
+        let opponent_scissors = "C";
+        let my_lose = "X";
+        let my_draw = "Y";
+        let my_win = "Z";
+
+        if opponent_hand == opponent_rock && my_hand == my_lose {
+            my_score += 3;
+        } else if opponent_hand == opponent_rock && my_hand == my_draw {
+            my_score += 4;
+        } else if opponent_hand == opponent_rock && my_hand == my_win {
+            my_score += 8;
+        } else if opponent_hand == opponent_paper && my_hand == my_lose {
+            my_score += 1;
+        } else if opponent_hand == opponent_paper && my_hand == my_draw {
+            my_score += 5;
+        } else if opponent_hand == opponent_paper && my_hand == my_win {
+            my_score += 9;
+        } else if opponent_hand == opponent_scissors && my_hand == my_lose {
+            my_score += 2;
+        } else if opponent_hand == opponent_scissors && my_hand == my_draw {
+            my_score += 6;
+        } else if opponent_hand == opponent_scissors && my_hand == my_win {
+            my_score += 7;
+        } else {
             println!("Problem mathing hand")
         }
 

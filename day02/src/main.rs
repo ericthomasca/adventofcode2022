695cfe15856a92f1d6468707cf52e41c546652d2
Eric Thomas <eric@ericthomas.ca>
2022-12-02 18:53:22 -0330

    compare hands, calculate scores

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -9,11 +9,79 @@ fn main() -> Result<()> {
 
     let rounds: Vec<&str> = contents.split('\n').collect();
 
-    for round in rounds {
-        let player: Vec<&str> = round.split(" ").collect();
-        println!("{:?}", player);
-    }
+    let mut player_1_total_score = 0;
+    let mut player_2_total_score = 0;
 
+    for round in rounds {
+        let hands: Vec<&str> = round.split(' ').collect();
+        let player_1_hand = hands[0];
+        let player_2_hand = hands[1];
+
+        let mut player_1_score = 0;
+        let mut player_2_score = 0;
+
+        if player_1_hand == "A" && player_2_hand == "X" {
+            player_1_score += 1;
+            player_2_score += 1;
+            player_1_score += 3;
+            player_2_score += 3;
+        }
+        else if player_1_hand == "A" && player_2_hand == "Y" {
+            player_1_score += 1;
+            player_2_score += 2;
+            player_1_score += 0;
+            player_2_score += 6;
+        }
+        else if player_1_hand == "A" && player_2_hand == "Z" {
+            player_1_score += 1;
+            player_2_score += 3;
+            player_1_score += 6;
+            player_2_score += 0;
+        }
+        else if player_1_hand == "B" && player_2_hand == "X" {
+            player_1_score += 2;
+            player_2_score += 1;
+            player_1_score += 6;
+            player_2_score += 0;
+        }
+        else if player_1_hand == "B" && player_2_hand == "Y" {
+            player_1_score += 2;
+            player_2_score += 2;
+            player_1_score += 3;
+            player_2_score += 3;
+        }
+        else if player_1_hand == "B" && player_2_hand == "Z" {
+            player_1_score += 2;
+            player_2_score += 3;
+            player_1_score += 0;
+            player_2_score += 6;
+        }
+        else if player_1_hand == "C" && player_2_hand == "X" {
+            player_1_score += 3;
+            player_2_score += 1;
+            player_1_score += 6;
+            player_2_score += 0;
+        }
+        else if player_1_hand == "C" && player_2_hand == "Y" {
+            player_1_score += 3;
+            player_2_score += 2;
+            player_1_score += 6;
+            player_2_score += 0;
+        }
+        else if player_1_hand == "C" && player_2_hand == "Z" {
+            player_1_score += 3;
+            player_2_score += 3;
+            player_1_score += 3;
+            player_2_score += 3;
+        }
+        else {
+            println!("Problem mathing hand")
+        }
+
+
+        println!("{}", player_1_score);
+        println!("{}", player_2_score);
+    }
 
     Ok(())
 }

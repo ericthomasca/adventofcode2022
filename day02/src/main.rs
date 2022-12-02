5b24852ef6fdabeee79ef25fc47f33e26edd2a5e
Eric Thomas <eric@ericthomas.ca>
2022-12-02 19:09:04 +0000

    read contents of test file

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::Result;
+
+fn main() -> Result<()> {
+    let mut file = File::open("test_input.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    println!("{}", contents);
+
+    Ok(())
 }
